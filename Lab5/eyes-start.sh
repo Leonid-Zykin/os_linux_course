3f5677c83ee720756ccac7c4401f21fbb10f0132
Leonid-Zykin <leonidzykin263@gmail.com>
2025-10-01 22:42:11 +0300

    Lab 5 done

---
+++ Lab5/eyes-start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+period_s=$1
+
+period_ms=$((period_s*1000))
+
+icon=/home/leonidas/projects/itmo/os_linux_course/Lab5/images/icon.png
+
+notify-send -i $icon -t $period_ms "Time to relax, senpai!" "$period_s seconds"
---
+++ Lab5/eyes-stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+relax_time=5  
+icon=/home/leonidas/projects/itmo/os_linux_course/Lab5/images/icon.png
+sleep $relax_time
+notify-send -i $icon -t 5000 "Time to get back to work, senpai!" "Relax finished"
